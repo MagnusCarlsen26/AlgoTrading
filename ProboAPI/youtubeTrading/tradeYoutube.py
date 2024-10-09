29aa44a3c3a2a835eee35081188d0588003c707c
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-10-10 02:20:29 +0530

    Minor Change

--- ProboAPI/youtubeTrading/tradeYoutube.py
+++ ProboAPI/youtubeTrading/tradeYoutube.py
@@ -21,12 +21,8 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 configs = {
-    "CTRL - Official Trailer": {
-        "video_id": "KEfeMPGj-Dw",
-    
-    },
-    "Linkin Park - Heavy Is The Crown": {
-        "video_id": "5FrhtahQiRc",
+    "Vicky Vidya Ka Woh Wala - Mushkil Hai": {
+        "video_id": "Ez6jGMpdCNY",
     },
     "MrBeast - 100 Identical Twins" : {
         "video_id": "snX5YyflrGw"
@@ -34,12 +30,19 @@ configs = {
 
 }
 
-def getViews(video_id,target_count,view_rate,title, endTime,history_view_rate) :
-    
-    average_history_view_rate = 0
-    for i in range(-1,-4,-1):
-        average_history_view_rate += int(list(history_view_rate[i].values())[0])
-    average_history_view_rate = average_history_view_rate//3
+def getViews(eventId) :
+    eventInfo = buyBook( eventId )   
+    lastIndex = eventInfo['title'].rindex('\'') 
+    util = eventInfo['title'][ lastIndex+ 1 :  ].split()
+    title = eventInfo['title'][ eventInfo['title'].index('\'') + 1 : lastIndex ]
+    video_id = configs[title]["video_id"]
+    target_count = float(util[3][:-1])*1000000
+    endTime = datetime.strptime(util[6] + " " + util[7][:-1], "%I:%M %p").time()
+
+    # average_history_view_rate = 0
+    # for i in range(-1,-4,-1):
+        # average_history_view_rate += int(list(history_view_rate[i].values())[0])
+    # average_history_view_rate = average_history_view_rate//3
 
 
     while True:
@@ -49,13 +52,13 @@ def getViews(video_id,target_count,view_rate,title, endTime,history_view_rate) :
             f.write(f"curr views = {int(view_count):,}\n")
             f.write(f"target views = {int(target_count):,}\n")
             f.write(f"difference = {int(target_count - float(view_count)):,}\n")
-            f.write(f"Time stamp left = {calcTimeStepsLeft(endTime)}\n")
-            f.write(f"Average Required = {int(target_count - float(view_count))//calcTimeStepsLeft(endTime)}\n")
-            f.write(f"view rate = {int(average_history_view_rate):,}\n")
-            f.write(f"Prev view rate = {(history_view_rate[-1])}\n")
-            f.write(f"Prev view rate = {(history_view_rate[-2])}\n")
-            f.write(f"Prev view rate = {(history_view_rate[-3])}\n")
-        time.sleep(0.1)
+            f.write(f"Time stamp left = {calcTimeStepsLeft(endTime,5)}\n")
+            f.write(f"Average Required = {int(target_count - float(view_count))//calcTimeStepsLeft(endTime,5)}\n")
+            # f.write(f"view rate = {int(average_history_view_rate):,}\n")
+            # f.write(f"Prev view rate = {(history_view_rate[-1])}\n")
+            # f.write(f"Prev view rate = {(history_view_rate[-2])}\n")
+            # f.write(f"Prev view rate = {(history_view_rate[-3])}\n")
+        time.sleep(5)
 
 
 def saveCurrViews( eventId : int ):
@@ -78,8 +81,8 @@ def fetchQuestion( eventId : int ):
     yt_title = eventInfo['title'][ eventInfo['title'].index('\'') + 1 : lastIndex ]
     target_views = float(util[3][:-1])*1000000
     target_time = datetime.strptime(util[6] + " " + util[7][:-1], "%I:%M %p").time()
-    print(eventInfo['title'])
     logging.info(eventInfo['title'])
+    print(eventInfo['title'])
     time_left = calcTimeStepsLeft(target_time,delta=1)
     print(f"Time for event = {time_left} minutes")
     logging.info(f"Time for event = {time_left} minutes")
@@ -100,4 +103,9 @@ def trade( topicId : list[int] ):
         logging.info("Error in trade()")
         print(e)
         logging.info(e)
-trade(452)
+
+eventId = int(input("Enter Event Id : "))
+while True :
+    # fetchQuestion(eventId)
+    getViews(eventId)
+# trade(452)
