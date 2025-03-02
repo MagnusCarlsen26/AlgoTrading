e9555089726825c3eaaf606849331e77a3e63adf
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-03-02 17:07:12 +0530

    Improved rush code

--- src/getOrderbook.rs
+++ src/getOrderbook.rs
@@ -10,26 +10,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let start_time = Instant::now();
 
-    let client = reqwest::Client::new();
-    
-    let mut headers = HeaderMap::new();
-    
-    headers.insert("accept", HeaderValue::from_static("*/*"));
-    headers.insert("accept-language", HeaderValue::from_static("en"));
-    headers.insert("appid", HeaderValue::from_static("in.probo.pro"));
-    headers.insert("authorization", HeaderValue::from_str(&format!("Bearer {}", env::var("PROBO_BEARER_TOKEN")?)).unwrap());
-    headers.insert("content-type", HeaderValue::from_static("application/json"));
+    // Create client with headers in one go
+    let client = reqwest::Client::builder()
+        .default_headers({
+            let mut headers = HeaderMap::new();
+            headers.insert("accept", HeaderValue::from_static("*/*"));
+            headers.insert("accept-language", HeaderValue::from_static("en"));
+            headers.insert("appid", HeaderValue::from_static("in.probo.pro"));
+            headers.insert("authorization", HeaderValue::from_str(&format!("Bearer {}", env::var("PROBO_BEARER_TOKEN")?)).unwrap());
+            headers.insert("content-type", HeaderValue::from_static("application/json"));
+            headers
+        })
+        .build()?;
 
     let response = client
         .get("https://prod.api.probo.in/api/v3/tms/trade/bestAvailablePrice?eventId=3752921")
-        .headers(headers)
         .send()
         .await?;
     
     let _data: Value = response.json().await?;
     
     let duration = start_time.elapsed();
-    println!("Request took: {:?}", duration);
+    println!("Request took: {:?} milliseconds", duration.as_millis());
     
     // println!("{:#?}", data);
     
