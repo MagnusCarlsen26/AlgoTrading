ecb0c8ba1d50bf4955981e0dc067faea0e181eea
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-03-02 14:15:50 +0530

    Clean Slate

--- Testing/utility/findhigherBuy.py
+++
@@ -1,9 +0,0 @@
-'''
-
-Input  : Order-book without Time stamp , Index for Buying , Weights array
-Output : Sum of weighted Buy Quantity higher than buy price 
-
-'''
-import numpy as np
-def findhigherBuy(lst,lowest_index):
-    return lst[lowest_index + 1 : lowest_index+6].sum()
